112b59b9414d26afec809de3886f779dfa053739
JunhOpportunity <twinjyjh5@gmail.com>
2025-10-25 04:24:28 +0900

    feat($자동차이름) : 자동차 이름 입력을 받기 위한 함수 선언

--- src/App.js
+++ src/App.js
@@ -4,6 +4,16 @@ class App {
   async run() {
     let carCount = 0;
     let racingResult = [];
+
+    const CAR_NAMES = await this.inputCarNames();
+
+  async inputCarNames() {
+    const CAR_NAMES = await MissionUtils.Console.readLineAsync(
+      "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)"
+    );
+
+    return CAR_NAMES;
+  }
 }
 
 export default App;
